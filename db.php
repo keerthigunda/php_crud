787cc2c8ec76dcb98e46ab3179413d7e29c12e97
Keerthi Gunda <KGunda@chenoainc.com>
2022-08-12 19:01:22 +0530

    adding azure db connections

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-$con=mysqli_init(); 
-mysqli_ssl_set($con, NULL, NULL, '/var/www/html/BaltimoreCyberTrustRoot.crt.pem', NULL, NULL); 
-mysqli_real_connect($con, "app-gk.mysql.database.azure.com", "kgunda@app-gk", "Shanmukha@1992", "php_mysql_crud", 3306);
+$conn=mysqli_init(); 
+mysqli_ssl_set($conn, NULL, NULL, '/var/www/html/BaltimoreCyberTrustRoot.crt.pem', NULL, NULL); 
+mysqli_real_connect($conn, "app-gk.mysql.database.azure.com", "kgunda@app-gk", "Shanmukha@1992", "php_mysql_crud", 3306);
 
 ?>
