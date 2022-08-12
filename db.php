ebd122f19e5379041fcff785a5ff4dc1cedb18be
Keerthi Gunda <KGunda@chenoainc.com>
2022-08-12 20:04:42 +0530

    adding db ssl cert

--- db.php
+++ db.php
@@ -2,6 +2,8 @@
 session_start();
 $conn=mysqli_init(); 
 mysqli_ssl_set($conn, NULL, NULL, '/var/www/html/BaltimoreCyberTrustRoot.crt.pem', NULL, NULL); 
-mysqli_real_connect($conn, "app-gk.mysql.database.azure.com", "kgunda@app-gk", "Shanmukha@1992", "php_mysql_crud", 3306);
-
+mysqli_real_connect($conn, "app-gk.mysql.database.azure.com", "kgunda@app-gk", "Shanmukha@1992", "php_mysql_crud", 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 ?>
