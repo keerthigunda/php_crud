6f73bd0f9131786e5599cf24af6a9bdb8dcb9d7e
Keerthi Gunda <KGunda@chenoainc.com>
2022-08-17 10:04:47 +0530

    removed adding crt file

--- db.php
+++ db.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-include('html/BaltimoreCyberTrustRoot.crt.pem');
 $conn=mysqli_init(); 
 mysqli_ssl_set($conn, NULL, NULL, '/www/html/BaltimoreCyberTrustRoot.crt.pem', NULL, NULL); 
 mysqli_real_connect($conn, "app-gk.mysql.database.azure.com", "kgunda@app-gk", "Shanmukha@1992", "php_mysql_crud", 3306, MYSQLI_CLIENT_SSL);
