b4dde8d6f098b955603af3b0f33a1d620bddf6c7
Keerthi Gunda <KGunda@chenoainc.com>
2022-08-17 17:58:56 +0530

    Modified DB credentials for new DB connection

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 session_start();
 $conn=mysqli_init(); 
 mysqli_ssl_set($conn, NULL, NULL, '/home/site/wwwroot/BaltimoreCyberTrustRoot.crt.pem', NULL, NULL); 
-mysqli_real_connect($conn, "app-gk.mysql.database.azure.com", "kgunda@app-gk", "Shanmukha@1992", "php_mysql_crud", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "practice-php-database.mysql.database.azure.com", "kgunda@practice-php-database", "Shanmukha@1992", "php_mysql_crud", 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
