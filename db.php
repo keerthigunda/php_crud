b84737e51908e8b1ca2f3d335adc98c7436f7a03
Keerthi Gunda <KGunda@chenoainc.com>
2022-08-17 11:16:05 +0530

    added new db

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 session_start();
 $conn=mysqli_init(); 
 mysqli_ssl_set($conn, NULL, NULL, '/www/html/BaltimoreCyberTrustRoot.crt.pem', NULL, NULL); 
-mysqli_real_connect($conn, "app-gk.mysql.database.azure.com", "kgunda@app-gk", "Shanmukha@1992", "php_mysql_crud", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "keerthi-server.mysql.database.azure.com", "yqrolaabei", "BK68C61C3TN6MPI6$", "keerthi-database", 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
